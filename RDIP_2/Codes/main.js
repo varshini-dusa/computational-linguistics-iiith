c46a2e129917e5f73338c56d8635a4b2f7972419
varshini-dusa <varshinidusa245@gmail.com>
2020-07-20 22:34:05 +0530

    U5 task2

---
+++ RDIP_2/Codes/main.js
@@ -0,0 +1,15 @@
+var engCorpus = [
+  "The child liked the chocolate",
+  "She was stopped by the bravest knight",
+  "Mary baked a cake for his birthday",
+  "She decorated the cake carefully",
+  "Mary wore a dress with polka dots",
+];
+
+var hindiCorpus = [
+  "राम ने सीता के लिए फल तोड़ा।",
+  "छोटे बच्चे पाठशाला जल्दी आयेंगे।",
+  "मेहनत का फल मीठा होता है।",
+  "वाह! वह खूबसूरत है।",
+  "पेड़ से पत्ते गिर गए।",
+];
