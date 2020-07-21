e724e0e60f1d3c861adccef40cb722fe3b152f0f
varshini-dusa <varshinidusa245@gmail.com>
2020-07-21 20:29:51 +0530

    U5 task3

--- RDIP_2/Codes/main.js
+++ RDIP_2/Codes/main.js
@@ -13,3 +13,33 @@ var hindiCorpus = [
   "वाह! वह खूबसूरत है।",
   "पेड़ से पत्ते गिर गए।",
 ];
+
+var selectedLanguage;
+
+function lanSelect() {
+  selectedLanguage = document.getElementById("lan").value;
+  var option;
+  if (selectedLanguage == "English") {
+    for (var i = 0; i < engCorpus.length; i++) {
+      option += "<option >" + engCorpus[i] + "</option>";
+    }
+    document.getElementById("CorpSelector").innerHTML =
+      "<select name='corp' id='corp' class='form-control align-self-center mx-auto' onchange='corpSelect()'><option selected>---Select a sentence---</option>" +
+      option +
+      "</select>";
+
+    document.getElementById("CorpSelector").innerHTML += "</select>";
+  } else if (selectedLanguage == "Hindi") {
+    for (var i = 0; i < hindiCorpus.length; i++) {
+      option += "<option value='" + i + "'>" + hindiCorpus[i] + "</option>";
+    }
+    document.getElementById("CorpSelector").innerHTML =
+      "<select name='corp' id='corp' class='form-control align-self-center mx-auto' onchange='corpSelect()'><option selected>---Select a sentence---</option>" +
+      option +
+      "</select>";
+  } else {
+    alert("Choose language");
+  }
+}
+
+function corpSelect() {}
